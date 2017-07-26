1c644104e607d08d6ae756f1a0b07fbb74e404f3
robin <robin@appledeMacBook-Air.local>
2017-07-26 12:16:48 -0700

    update to info.plist

--- android/app/src/main/java/com/nutritiondoctor/MainApplication.java
+++ android/app/src/main/java/com/nutritiondoctor/MainApplication.java
@@ -3,10 +3,8 @@ package com.nutritiondoctor;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.github.xfumihiro.react_native_image_to_base64.ImageToBase64Package;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.rnfs.RNFSPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RCTCameraPackage(),
-            new ImageToBase64Package(),
             new VectorIconsPackage(),
-            new RNFSPackage()
+            new RCTCameraPackage()
       );
     }
   };
